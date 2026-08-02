eee916aecc86e1767b40edbc1f3ccbd8beb8e259
CHARAN_SIRIPIREDDY <123306640+charan0003@users.noreply.github.com>
2026-08-02 18:28:51 +0530

    Time: 99 ms (84.13%), Space: 61.3 MB (52.85%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -7,12 +7,10 @@ class Solution:
             if len(comb) == k:
                 res.append(comb.copy())
                 return
-
             for i in range(start, n + 1):
                 comb.append(i)
                 backtrack(i + 1, comb)
                 comb.pop()
 
         backtrack(1, [])
-
         return res
