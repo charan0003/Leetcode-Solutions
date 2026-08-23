bad15e5883caaae8c535786afe4d7dfd86843a1f
CHARAN_SIRIPIREDDY <123306640+charan0003@users.noreply.github.com>
2026-08-23 21:10:54 +0530

    Time: 56 ms (63.53%), Space: 19.8 MB (65.29%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.py
@@ -0,0 +1,31 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+
+        
+        n = len(num) // 2
+
+        left_sum = 0
+        right_sum = 0
+        left_q = 0
+        right_q = 0
+
+        for i in range(n):
+            if num[i] == "?":
+                left_q += 1
+            else:
+                left_sum += int(num[i])
+
+        for i in range(n, len(num)):
+            if num[i] == "?":
+                right_q += 1
+            else:
+                right_sum += int(num[i])
+
+        
+        if (left_q + right_q) % 2 == 1:
+            return True
+
+        diff = left_sum - right_sum
+
+        
+        return 2 * diff != 9 * (right_q - left_q)
